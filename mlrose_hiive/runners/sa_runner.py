c7cd1d25292c50ae718d74e723d0651d57e54ee0
Alson Yap <jyap30@gatech.edu>
2020-02-22 08:36:31 +0800

    added temperature decay for SA runner

--- mlrose_hiive/runners/sa_runner.py
+++ mlrose_hiive/runners/sa_runner.py
@@ -17,24 +17,36 @@ Example usage:
                   temperature_list=[1, 10, 50, 100, 250, 500, 1000, 2500, 5000, 10000])
 
     # the two data frames will contain the results
-    df_run_stats, df_run_curves = sa.run()                  
+    df_run_stats, df_run_curves = sa.run()
 """
 
 
 @short_name('sa')
 class SARunner(_RunnerBase):
 
-    def __init__(self, problem, experiment_name, seed, iteration_list, temperature_list, decay_list=None,
+    def __init__(self, problem, experiment_name,
+                 seed, iteration_list,
+                 temperature_list,
+                 temperature_decay_list,
+                 decay_list=None,
                  max_attempts=500, generate_curves=True, **kwargs):
-        super().__init__(problem=problem, experiment_name=experiment_name, seed=seed, iteration_list=iteration_list,
-                         max_attempts=max_attempts, generate_curves=generate_curves,
+        super().__init__(problem=problem,
+                         experiment_name=experiment_name,
+                         seed=seed,
+                         iteration_list=iteration_list,
+                         max_attempts=max_attempts,
+                         generate_curves=generate_curves,
                          **kwargs)
         self.temperature_list = temperature_list
+        self.temperature_decay_list = temperature_decay_list
         if decay_list is None:
             decay_list = [mlrose_hiive.GeomDecay]
         self.decay_list = decay_list
 
     def run(self):
-        temperatures = [decay(init_temp=t) for t in self.temperature_list for decay in self.decay_list]
+        temperatures = [decay(init_temp=t, decay=d)
+                        for t in self.temperature_list
+                        for d in self.temperature_decay_list
+                        for decay in self.decay_list]
         return super().run_experiment_(algorithm=mlrose_hiive.simulated_annealing,
                                        schedule=('Temperature', temperatures))
